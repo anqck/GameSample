c3599a384297d6eb47897065a2116ac3061ab9cd
Khanh An <Khanh An@DESKTOP-QJVR5QD>
2017-09-27 16:20:58 +0700

    ACC

--- sample01/main.cpp
+++ sample01/main.cpp
@@ -87,7 +87,7 @@ void GameRun(HWND hWnd)
 	if (d3ddv->BeginScene()) 
 	{
 		// Clear back buffer with BLACK
-		d3ddv->ColorFill(back_buffer,NULL,D3DCOLOR_XRGB(255,255,0));
+		d3ddv->ColorFill(back_buffer,NULL,D3DCOLOR_XRGB(255,255,255));
 		
 		// Random color for the surface
 	/*	int r = rand() % 255; 
